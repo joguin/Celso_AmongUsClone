438d4b8c7b14f2f7fb810c4e7738d3c3f8718484
Celso Alexandre <celsoalexandre@live.com>
2020-11-07 21:48:27 -0300

    Video 2

---
+++ Assets/Scripts/AU_PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AU_PlayerController : MonoBehaviour
+{
+    //Components
+    Rigidbody myRB;
+    Transform myAvatar;
+    //Player movement
+    [SerializeField] InputAction WASD;
+    Vector2 movementInput;
+    [SerializeField] float movementSpeed;
+
+    private void OnEnable()
+    {
+        WASD.Enable();
+    }
+
+    private void OnDisable()
+    {
+        WASD.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+        myAvatar = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInput = WASD.ReadValue<Vector2>();
+
+        if (movementInput.x != 0)
+        {
+            myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        myRB.velocity = movementInput * movementSpeed;
+    }
+}
